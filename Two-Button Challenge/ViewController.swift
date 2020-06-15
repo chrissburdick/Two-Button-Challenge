4676c6a5c91afc2c08d6c676fed881c4bc27e6fa
Chris Burdick <burdicch@bc.edu>
2020-06-15 11:52:01 -0400

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Chris Burdick on 6/15/20.
+//  Copyright © 2020 Chris Burdick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
